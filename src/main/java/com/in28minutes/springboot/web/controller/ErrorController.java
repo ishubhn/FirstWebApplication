e96ad16db2239abda6cbb4605327825490306467
ishubhn <imshubhn@gmail.com>
2020-09-01 16:35:36 +0530

    Exception handling via Spring

---
+++ src/main/java/com/in28minutes/springboot/web/controller/ErrorController.java
@@ -0,0 +1,22 @@
+package com.in28minutes.springboot.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller("error")
+public class ErrorController {
+	@ExceptionHandler(Exception.class)
+	public ModelAndView handleException(HttpServletRequest request, Exception ex) {
+		ModelAndView view = new ModelAndView();
+		view.addObject("exception", ex.getStackTrace());
+		view.addObject("url", request.getRequestURL());
+		view.setViewName("error");
+		return view;
+	}
+}
+
+//model + view = we can store both model and view details
