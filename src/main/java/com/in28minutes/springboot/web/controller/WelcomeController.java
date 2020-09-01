18398788b56eed34370f5ef0ab556f2bf43c0906
ishubhn <imshubhn@gmail.com>
2020-09-01 15:19:49 +0530

    SpringSecurity get userName

--- src/main/java/com/in28minutes/springboot/web/controller/WelcomeController.java
+++ src/main/java/com/in28minutes/springboot/web/controller/WelcomeController.java
@@ -1,44 +1,55 @@
 package com.in28minutes.springboot.web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.SessionAttributes;
-
-import com.in28minutes.springboot.web.service.LoginService;
-
-@Controller
-@SessionAttributes("name")
-public class LoginController {
-	@Autowired					/*wired to LoginService as it has @Component declared and matches with LoginService 
-									name here, so spring knew that is needs to create instance obj*/
-	LoginService service;
-	
-	@RequestMapping(value="/", method = RequestMethod.GET)
-	public String showLogin(ModelMap model) {
-		model.put("name", "Shubham");
-		return "welcome";
-	}
-	
-}
-
-
-//	@ResponseBody			/*to send string back to browser instead of jsp*/
-//	@RequestMapping(value="/login", method = RequestMethod.GET)
-//	@RequestMapping("/login")
-//	public String loginMessage(ModelMap model) {
-////		model.put("name", name);
-////		model.put("password",pass);
-//		return "login";
-//		
-//	}
-
 
 /*
+*import org.springframework.beans.factory.annotation.Autowired;
+*import org.springframework.web.bind.annotation.RequestParam;
+*import org.springframework.web.bind.annotation.ResponseBody;
+*import org.springframework.web.bind.annotation.SessionAttributes;
+*import com.in28minutes.springboot.web.service.LoginService;
+*@Controller
+*@SessionAttributes("name") //gettin 'userName" fron Spring Security principal
+*public class WelcomeController {
+*@Autowired
+*		 wired to LoginService as it has @Component declared and matches with
+*		 LoginService name here, so spring knew that is needs to create instance obj
+*LoginService service;
+*/
+
+@Controller
+public class WelcomeController {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String showWelcomePage(ModelMap model) {
+		model.put("name", getLoggedinUserName());
+		return "welcome";
+	}
+
+	private String getLoggedinUserName() {
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+		if (principal instanceof UserDetails) {
+			return ((UserDetails) principal).getUsername();
+		}
+
+		return principal.toString();
+	}
+
+}
+
+/*
+ * @ResponseBody /*to send string back to browser instead of jsp
+ * 
+ * @RequestMapping(value="/login", method = RequestMethod.GET)
+ * 
+ * @RequestMapping("/login") public String loginMessage(ModelMap model) {
+ * model.put("name", name); model.put("password",pass); return "login"; }
+ * 
  * @RequestMapping(value="/login", method = RequestMethod.POST) public String
  * doLogin(ModelMap model, @RequestParam String name, @RequestParam String pass)
  * { boolean isValidUser = service.validateUser(name, pass);
